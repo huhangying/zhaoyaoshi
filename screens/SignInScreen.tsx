cb0a84552fc2ca1031c5dfa6a7b364c98ef2a2d6
Harry Hu <harry.hu@yahoo.ca>
2020-12-07 23:27:35 -0500

    enhance login

--- screens/SignInScreen.tsx
+++ screens/SignInScreen.tsx
@@ -38,31 +38,36 @@ export default function SignInScreen() {
   }
 
   return (
-    <View style={styles.container}>
-      <Input
-        placeholder="用户名"
-        leftIcon={{ type: 'ionicon', name: 'md-person', color: '#2f95dc', size: 30 }}
-        style={styles.inputPadding}
-        value={username}
-        onChangeText={setUsername} autoCompleteType='name'
-      />
-      <Input
-        placeholder="密码"
-        leftIcon={{ type: 'ionicon', name: 'md-lock', color: '#2f95dc', size: 30 }}
-        style={styles.inputPadding}
-        value={password}
-        onChangeText={setPassword}
-        secureTextEntry
-      />
-      <>
-        {hasError ?
-          <Text style={styles.errorMsg}>{errorMessage}</Text> : null
-        }
-      </>
-      <View>
-        <Button title="登录" onPress={() => login(username, password)} titleStyle={styles.paddingX} containerStyle={styles.submitStyle} />
+    <>
+      <View style={styles.container}>
+        <Input
+          placeholder="用户名"
+          leftIcon={{ type: 'ionicon', name: 'md-person', color: '#2f95dc', size: 30 }}
+          style={styles.inputPadding}
+          value={username}
+          onChangeText={setUsername} autoCompleteType='name'
+        />
+        <Input
+          placeholder="密码"
+          leftIcon={{ type: 'ionicon', name: 'md-lock', color: '#2f95dc', size: 30 }}
+          style={styles.inputPadding}
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry
+        />
+        <>
+          {hasError ?
+            <Text style={styles.errorMsg}>{errorMessage}</Text> : null
+          }
+        </>
+        <View style={{ paddingVertical: 16 }}>
+          <Button title="登录" onPress={() => login(username, password)} />
+        </View>
       </View>
-    </View>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>hwem</Text>
+      </View>
+    </>
   );
 }
 
@@ -71,9 +76,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    justifyContent: 'center',
-    paddingHorizontal: 30,
-    paddingBottom: 50,
+    justifyContent: 'flex-start',
+    paddingHorizontal: 40,
+    paddingTop: 120,
+    paddingBottom: 60,
   },
   inputPadding: {
     paddingHorizontal: 10,
@@ -82,9 +88,6 @@ const styles = StyleSheet.create({
     margin: 20,
     flexDirection: "row",
     justifyContent: "flex-end"
-  },
-  paddingX: {
-    paddingHorizontal: 30,
   },
   errorMsg: {
     color: 'red',
