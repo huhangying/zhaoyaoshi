551200dc126a383aab6f92a9fcd6d7611b1c0618
Harry Hu <harry.hu@yahoo.ca>
2020-12-08 00:18:32 -0500

    add emoji padding

--- components/ChatItem.tsx
+++ components/ChatItem.tsx
@@ -36,7 +36,7 @@ export default function ChatItem({ chat, doctor, icon }: { chat: Chat, doctor: D
                 <View style={{ flex: 1, flexDirection: 'row', backgroundColor: 'transparent' }}>
                   {parseChatData(chat.data).map((text, i) => (isEmoji(text) ? (
                     <Image key={i}
-                      source={{ uri: (getEmojiPath(text)) }} style={{ width: 20, height: 20 }} />
+                      source={{ uri: (getEmojiPath(text)) }} style={{ width: 20, height: 20, marginHorizontal: 2 }} />
                   ) : (
                       <Text key={i}>
                         {text}
