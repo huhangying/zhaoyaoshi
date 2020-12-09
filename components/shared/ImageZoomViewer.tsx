65bef0b5c299a07722ac1f23a9fcb025df3e8b56
Harry Hu <harry.hu@yahoo.ca>
2020-12-09 00:38:05 -0500

    fix ios image viewer close

--- components/shared/ImageZoomViewer.tsx
+++ components/shared/ImageZoomViewer.tsx
@@ -6,22 +6,25 @@ import ImageViewer from 'react-native-image-zoom-viewer';
 export default function ImageZoomViewer({ visible, img, title, onClose }: { visible: boolean, img: any, title?: string, onClose: any }) {
   return (
     <Modal visible={visible} transparent={true}>
-      <ImageViewer
+      <ImageViewer style={{ flex: 1 }}
         imageUrls={[{ url: img }]} enableSwipeDown={true} onCancel={onClose}
         saveToLocalByLongPress={false}
         renderIndicator={(currentIndex, allSize) => <Text></Text>}
-        renderHeader={(currentIndex) => (<SafeAreaView style={{ flex: 1, flexDirection: 'column', justifyContent: 'space-between' }}>
-          {Platform.OS === "ios" ? (<>
-            <Pressable onPress={onClose}>
-              <Text style={{ color: 'white', top: 4, position: 'absolute', alignSelf: 'center', paddingHorizontal: 20, marginTop: -20 }}>
+        renderHeader={(currentIndex) => (<>
+          {Platform.OS === "ios" ? (
+            <View style={{ flex: 1, flexDirection: 'column', zIndex: 999, justifyContent: 'space-between' }}>
+              <Text style={{ color: 'white', top: 44, position: 'absolute', alignSelf: 'center' }}>
                 {title}</Text>
-            </Pressable>
-          </>) : (<>
-            <Text style={{ color: 'white', top: 2, position: 'absolute', alignSelf: 'center', zIndex: 999, marginTop: 24 }}>{title}</Text>
-            <Ionicons name="ios-close-circle-outline" size={28} color="white" onPress={onClose}
-              style={{ alignSelf: 'flex-end', marginTop: 20, paddingHorizontal: 20, zIndex: 999 }} />
-          </>)}
-        </SafeAreaView>)}
+              <Ionicons name="ios-close-circle-outline" size={28} color="white" onPress={onClose}
+                style={{ alignSelf: 'flex-end', position: 'absolute', marginTop: 40, right: 0, paddingHorizontal: 20 }} />
+            </View>
+          ) : (
+              <View style={{ flex: 1, flexDirection: 'column', justifyContent: 'space-between' }}>
+                <Text style={{ color: 'white', top: 2, position: 'absolute', alignSelf: 'center', zIndex: 999, marginTop: 24 }}>{title}</Text>
+                <Ionicons name="ios-close-circle-outline" size={28} color="white" onPress={onClose}
+                  style={{ alignSelf: 'flex-end', marginTop: 20, paddingHorizontal: 20, zIndex: 999 }} />
+              </View>)}
+        </>)}
       />
     </Modal>
   );
