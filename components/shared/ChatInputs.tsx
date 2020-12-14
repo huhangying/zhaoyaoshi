e306aa8b15d94a7b433b27ab2830fc70cac03636
Harry Hu <harry.hu@yahoo.ca>
2020-12-13 23:18:48 -0500

    refactor shared chat

---
+++ components/shared/ChatInputs.tsx
@@ -0,0 +1,155 @@
+import { Ionicons } from "@expo/vector-icons";
+import * as ImagePicker from 'expo-image-picker';
+import React, { useCallback, useEffect, useState } from "react";
+import { Platform, SafeAreaView, StyleSheet } from "react-native";
+import { Button, Input } from "react-native-elements";
+import { tap } from "rxjs/operators";
+import { Doctor } from "../../models/crm/doctor.model";
+import { createBlobFormData, uploadDoctorDir } from "../../services/core/upload.service";
+import EmojiMenu from "../chat/emojiMenu";
+import ShortcutBottomMenu from "../chat/ShortcutsBottomMenu";
+import { View } from "../Themed";
+
+export default function ChatInputs({ pid, doctor, onSend }: { pid: string, doctor?: Doctor, onSend: any }) {
+  const [isShortcutsMenuVisible, setIsShortcutsMenuVisible] = useState(false);
+  const [inputText, setInputText] = useState('')
+  const [showEmojis, setShowEmojis] = useState(false)
+
+  useEffect(() => {
+    // effect
+
+    (async () => {
+      if (Platform.OS !== 'web') {
+        const { status } = await ImagePicker.requestCameraRollPermissionsAsync();
+        if (status !== 'granted') {
+          alert('您禁止使用本地图片功能');
+        }
+        const permission = await ImagePicker.requestCameraPermissionsAsync();
+        if (permission.status !== 'granted') {
+          alert('您禁止使用照相机功能');
+        }
+      }
+    })();
+
+    return () => {
+      // cleanup
+    }
+  }, [doctor])
+
+  const onShortcutSelected = useCallback((shortcut) => {
+    if (shortcut) {
+      setInputText(inputText + shortcut);
+    }
+    setIsShortcutsMenuVisible(false);
+  }, [inputText]);
+
+  const onEmojiSelected = useCallback((emoji) => {
+    setInputText(inputText + emoji);
+  }, [inputText]);
+
+  const showShortcutsMenu = () => {
+    setIsShortcutsMenuVisible(true);
+  };
+
+  const toggleEmojis = () => {
+    setShowEmojis(!showEmojis);
+  }
+
+  const pickImage = async () => {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 4],
+      quality: 0.7,
+    });
+
+    await setAndUploadImage(result);
+  };
+
+  const pickCamera = async () => {
+    const result = await ImagePicker.launchCameraAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 4],
+      quality: 0.7,
+    });
+
+    await setAndUploadImage(result);
+  };
+
+  const setAndUploadImage = async (result: ImagePicker.ImagePickerResult) => {
+    if (!result.cancelled) {
+      const formData = await createBlobFormData(result);
+      uploadDoctorDir(pid, 'chat', formData).pipe(
+        tap((result: { path: string }) => {
+          if (result?.path) {
+            // sendChatMsg(result.path, true); // send chat imagePath
+            onSend(result.path, true);
+          }
+        })
+      ).subscribe(() => {
+        // scrollToEnd();
+      });
+    }
+  }
+
+  const send = (msg: string) => {
+    setShowEmojis(false);
+    onSend(msg);
+    setInputText('');
+  }
+
+  return (
+    <SafeAreaView style={styles.fixBottom}>
+    <Input
+      placeholder="请输入..."
+      value={inputText}
+      onChangeText={setInputText}
+      style={styles.bottomInput}
+      multiline={true}
+      rightIcon={
+        <Button title="发送" containerStyle={{ marginRight: -12 }} buttonStyle={{ paddingLeft: 4, paddingRight: 10 }} icon={{ type: 'ionicon', name: 'ios-paper-plane', color: 'white' }}
+          disabled={!inputText} onPress={() => send(inputText)}
+        />}
+    />
+
+    <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-between', marginTop: -28, backgroundColor: 'lightgray', paddingVertical: 6, paddingHorizontal: 16 }}>
+      <View style={{ flex: 1, flexDirection: 'row', alignItems: 'center', backgroundColor: 'lightgray' }}>
+        <Ionicons name="ios-happy" size={26} color="#0095ff"
+          style={{ marginRight: 22, color: !showEmojis ? '#0095ff' : 'orange' }} onPress={toggleEmojis}></Ionicons>
+        <Ionicons name="ios-image" size={26} color="#0095ff" style={styles.mr3} onPress={pickImage}></Ionicons>
+        <Ionicons name="ios-camera" size={26} color="#0095ff" style={styles.mr3} onPress={pickCamera}></Ionicons>
+        <Ionicons name="ios-undo" size={26} color="#0095ff" onPress={showShortcutsMenu}></Ionicons>
+      </View>
+      <View style={{ flex: 1, flexDirection: 'row', alignItems: 'center', alignContent: 'center', backgroundColor: 'lightgray' }}>
+        <Button type="outline" raised={true} buttonStyle={{ paddingVertical: 2, paddingHorizontal: 16 }} title="标识完成"></Button>
+        <Button type="outline" raised={true} buttonStyle={{ paddingVertical: 2, paddingHorizontal: 16 }} title="返回付费咨询"></Button>
+      </View>
+    </View>
+    {!!showEmojis &&
+      <EmojiMenu onSelect={onEmojiSelected}></EmojiMenu>
+    }
+    {!!isShortcutsMenuVisible &&
+      <ShortcutBottomMenu shortcuts={doctor?.shortcuts || ''} onSelect={onShortcutSelected}></ShortcutBottomMenu>
+    }
+  </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  fixBottom: {
+    position: 'absolute',
+    right: 0,
+    left: 0,
+    bottom: 0,
+    backgroundColor: 'white',
+    paddingHorizontal: 0,
+    marginHorizontal: 0,
+  },
+  bottomInput: {
+    paddingHorizontal: 2,
+  },
+  mr3: {
+    marginRight: 22,
+  }
+});
--- screens/consult/ChatScreen.tsx
+++ screens/consult/ChatScreen.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
-import { Keyboard, KeyboardAvoidingView, Platform, SafeAreaView, ScrollView, StyleSheet, useWindowDimensions } from 'react-native';
-import { Text, View } from '../../components/Themed';
-import { Button, Input } from 'react-native-elements';
+import { Keyboard, KeyboardAvoidingView, Platform, ScrollView, StyleSheet, useWindowDimensions } from 'react-native';
+import { View } from '../../components/Themed';
 import { useNavigation, useRoute } from '@react-navigation/native';
 import { useDispatch, useSelector } from 'react-redux';
 import { AppState } from '../../models/app-state.model';
@@ -14,14 +13,10 @@ import { getUserDetailsById } from '../../services/user.service';
 import { User } from '../../models/crm/user.model';
 import { imgPath } from '../../services/core/image.service';
 import { UpdateHideBottomBar } from '../../services/core/app-store.actions';
-import { Ionicons } from '@expo/vector-icons';
-import ShortcutBottomMenu from '../../components/chat/ShortcutsBottomMenu';
-import EmojiMenu from '../../components/chat/emojiMenu';
 import { NotificationType } from '../../models/io/notification.model';
 import Spinner from '../../components/shared/Spinner';
-import * as ImagePicker from 'expo-image-picker';
 import ImageZoomViewer from '../../components/shared/ImageZoomViewer';
-import { createBlobFormData, uploadDoctorDir } from '../../services/core/upload.service';
+import ChatInputs from '../../components/shared/ChatInputs';
 
 export default function ChatScreen() {
   const scrollViewRef = useRef<ScrollView>();
@@ -37,9 +32,6 @@ export default function ChatScreen() {
   const initUser: User = { _id: '' };
   const [user, setUser] = useState(initUser);
   const dispatch = useDispatch();
-  const [isShortcutsMenuVisible, setIsShortcutsMenuVisible] = useState(false);
-  const [inputText, setInputText] = useState('')
-  const [showEmojis, setShowEmojis] = useState(false)
   const navigation = useNavigation();
 
   // 监听呼入消息
@@ -76,19 +68,6 @@ export default function ChatScreen() {
       ).subscribe();
     }
 
-    (async () => {
-      if (Platform.OS !== 'web') {
-        const { status } = await ImagePicker.requestCameraRollPermissionsAsync();
-        if (status !== 'granted') {
-          alert('您禁止使用本地图片功能');
-        }
-        const permission = await ImagePicker.requestCameraPermissionsAsync();
-        if (permission.status !== 'granted') {
-          alert('您禁止使用照相机功能');
-        }
-      }
-    })();
-
     Keyboard.addListener("keyboardDidShow", scrollToEnd);
     dispatch(UpdateHideBottomBar(true));
 
@@ -102,48 +81,7 @@ export default function ChatScreen() {
     scrollViewRef.current?.scrollToEnd({ animated: true });
   }
 
-  const onShortcutSelected = useCallback((shortcut) => {
-    if (shortcut) {
-      setInputText(inputText + shortcut);
-    }
-    setIsShortcutsMenuVisible(false);
-  }, [inputText]);
-
-  const onEmojiSelected = useCallback((emoji) => {
-    setInputText(inputText + emoji);
-  }, [inputText]);
-
-  const showShortcutsMenu = () => {
-    setIsShortcutsMenuVisible(true);
-  };
-
-  const toggleEmojis = () => {
-    setShowEmojis(!showEmojis);
-  }
-
-  const send = (data: string, isImg = false) => {
-    if (doctor) {
-      setShowEmojis(false);
-      switch (type) {
-        case NotificationType.chat:
-          sendChatMsg(data, isImg);
-          break;
-
-        // case NotificationType.adverseReaction:
-        // case NotificationType.doseCombination:
-        //   this.sendFeedback(imgPath);
-        //   break;
-
-        // case NotificationType.consultChat:
-        //   this.sendConsult(imgPath);
-        //   break;
-      }
-      setInputText('');
-      scrollToEnd();
-    }
-  }
-
-  const sendChatMsg = (data: string, isImg = false) => {
+  const onSend = useCallback((data: string, isImg = false) => {
     if (!doctor) {
       return;
     }
@@ -163,46 +101,8 @@ export default function ChatScreen() {
 
     ioService?.sendChat(doctor._id, chat);
     sendChat(chat).subscribe(); // chatService
-  }
-
-  const pickImage = async () => {
-    const result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
-      allowsEditing: true,
-      aspect: [4, 4],
-      quality: 0.7,
-    });
-
-    await setAndUploadImage(result);
-  };
-
-  const pickCamera = async () => {
-    const result = await ImagePicker.launchCameraAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
-      allowsEditing: true,
-      aspect: [4, 4],
-      quality: 0.7,
-    });
-
-    await setAndUploadImage(result);
-  };
-
-  const setAndUploadImage = async (result: ImagePicker.ImagePickerResult) => {
-    if (!result.cancelled) {
-      const formData = await createBlobFormData(result);
-      uploadDoctorDir(pid, 'chat', formData).pipe(
-        tap((result: { path: string }) => {
-          if (result?.path) {
-            sendChatMsg(result.path, true); // send chat imagePath
-          }
-        })
-      ).subscribe(() => {
-        scrollToEnd();
-      });
-    }
-  }
-
-
+    scrollToEnd();
+  }, [chats, doctor, ioService, pid]);
 
 
   const [isOpenViewer, setIsOpenViewer] = useState(false);
@@ -223,7 +123,7 @@ export default function ChatScreen() {
       <KeyboardAvoidingView style={{ flex: 1 }}
         behavior={Platform.OS === "ios" ? "position" : 'height'}
         keyboardVerticalOffset={Platform.OS === "ios" ? 88 : 96} >
-        <ScrollView ref={scrollViewRef} style={{ marginBottom: Platform.OS === "ios" ? 116 : 88, minHeight: dimensions.height - 190 }}
+        <ScrollView ref={scrollViewRef} style={{ marginBottom: Platform.OS === "ios" ? 102 : 88, minHeight: dimensions.height - 190 }}
           onContentSizeChange={scrollToEnd}>
           <View style={styles.chats}>
             {chats.map((chat, i) => (chat.sender === doctor._id ?
@@ -234,40 +134,8 @@ export default function ChatScreen() {
             }
           </View>
         </ ScrollView>
-        <SafeAreaView style={styles.fixBottom}>
-          <Input
-            placeholder="请输入..."
-            value={inputText}
-            onChangeText={setInputText}
-            style={styles.bottomInput}
-            multiline={true}
-            rightIcon={
-              <Button title="发送" containerStyle={{ marginRight: -12 }} buttonStyle={{ paddingLeft: 4, paddingRight: 10 }} icon={{ type: 'ionicon', name: 'ios-paper-plane', color: 'white' }}
-                disabled={!inputText} onPress={() => send(inputText)}
-              />}
-          />
-
-          <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-between', marginTop: -28, backgroundColor: 'lightgray', paddingVertical: 6, paddingHorizontal: 16 }}>
-            <View style={{ flex: 1, flexDirection: 'row', alignItems: 'center', backgroundColor: 'lightgray' }}>
-              <Ionicons name="ios-happy" size={26} color="#0095ff"
-                style={{ marginRight: 22, color: !showEmojis ? '#0095ff' : 'orange' }} onPress={toggleEmojis}></Ionicons>
-              <Ionicons name="ios-image" size={26} color="#0095ff" style={styles.mr3} onPress={pickImage}></Ionicons>
-              <Ionicons name="ios-camera" size={26} color="#0095ff" style={styles.mr3} onPress={pickCamera}></Ionicons>
-              <Ionicons name="ios-undo" size={26} color="#0095ff" onPress={showShortcutsMenu}></Ionicons>
-            </View>
-            <View style={{ flex: 1, flexDirection: 'row', alignItems: 'center', alignContent: 'center', backgroundColor: 'lightgray' }}>
-              <Button type="outline" raised={true} buttonStyle={{ paddingVertical: 2, paddingHorizontal: 16 }} title="标识完成"></Button>
-              <Button type="outline" raised={true} buttonStyle={{ paddingVertical: 2, paddingHorizontal: 16 }} title="返回付费咨询"></Button>
-            </View>
-          </View>
-          {!!showEmojis &&
-            <EmojiMenu onSelect={onEmojiSelected}></EmojiMenu>
-          }
-          {!!isShortcutsMenuVisible &&
-            <ShortcutBottomMenu shortcuts={doctor.shortcuts || ''} onSelect={onShortcutSelected}></ShortcutBottomMenu>
-          }
-          <ImageZoomViewer img={viewerImg} visible={isOpenViewer} onClose={closeViewer}></ImageZoomViewer>
-        </SafeAreaView>
+        <ChatInputs pid={pid} doctor={doctor} onSend={onSend}></ChatInputs>
+        <ImageZoomViewer img={viewerImg} visible={isOpenViewer} onClose={closeViewer}></ImageZoomViewer>
 
       </KeyboardAvoidingView>
     );
@@ -288,19 +156,4 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'column-reverse',
   },
-  fixBottom: {
-    position: 'absolute',
-    right: 0,
-    left: 0,
-    bottom: 0,
-    backgroundColor: 'white',
-    paddingHorizontal: 0,
-    marginHorizontal: 0,
-  },
-  bottomInput: {
-    paddingHorizontal: 2,
-  },
-  mr3: {
-    marginRight: 22,
-  }
 });
