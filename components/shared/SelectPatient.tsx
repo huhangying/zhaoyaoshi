7807a75f8083c526370edf043fff077447b453af
Harry Hu <harry.hu@yahoo.ca>
2021-04-05 11:52:09 -0400

    selectPatient add spinner

--- components/shared/SelectPatient.tsx
+++ components/shared/SelectPatient.tsx
@@ -4,12 +4,13 @@ import { useEffect, useRef, useState } from 'react';
 import { Text } from '../../components/Themed';
 import { getDoctorGroups, getPatientGroupedRelationships } from '../../services/doctor.service';
 import { GroupedRelationship, Relationship } from '../../models/crm/relationship.model';
-import { tap } from 'rxjs/operators';
+import { finalize, tap } from 'rxjs/operators';
 import { Caption, List } from 'react-native-paper';
 import { DoctorGroup } from '../../models/crm/doctor-group.model';
-import { white } from 'react-native-paper/lib/typescript/styles/colors';
+import Spinner from './Spinner';
 
 export default function SelectPatient({ doctorId, onSelect }: { doctorId?: string, onSelect: any }) {
+  const [loading, setLoading] = useState(false);
   const [groupedRelationships, setGroupedRelationships] = useState([]);
   const initDoctorGroup: DoctorGroup = { _id: '', name: '' };
   const [doctorGroups, setDoctorGroups] = useState([initDoctorGroup]);
@@ -19,12 +20,20 @@ export default function SelectPatient({ doctorId, onSelect }: { doctorId?: strin
 
   useEffect(() => {
     if (doctorId) {
+      setLoading(true);
       getPatientGroupedRelationships(doctorId).pipe(
-        tap(results => setGroupedRelationships(results))
+        tap(results => setGroupedRelationships(results)),
+        finalize(() => {
+          setLoading(false);
+        })
       ).subscribe();
 
+      setLoading(true);
       getDoctorGroups(doctorId).pipe(
-        tap(results => setDoctorGroups(results))
+        tap(results => setDoctorGroups(results)),
+        finalize(() => {
+          setLoading(false);
+        })
       ).subscribe();
     }
 
@@ -61,38 +70,42 @@ export default function SelectPatient({ doctorId, onSelect }: { doctorId?: strin
     scrollViewRef.current?.scrollTo({ x: 0, y: 0, animated: true });
   }
 
-  return (
-    <>
-      <Caption style={styles.m3}>从用户群组中选择</Caption>
-      <ScrollView ref={scrollViewRef}>
-        {doctorGroups.map((group: DoctorGroup, i) => (
-          <List.Accordion
-            style={styles.group}
-            left={props => <List.Icon {...props} icon="account-group" color="azure" />}
-            title={group.name}
-            titleStyle={{color: 'white'}}
-            id={i} key={i}
-            expanded={i === expandId}
-            onPress={() => handlePress(i, group)}>
+  if (!doctorId || loading) {
+    return (<Spinner />);
+  } else {
+    return (
+      <>
+        <Caption style={styles.m3}>从用户群组中选择</Caption>
+        <ScrollView ref={scrollViewRef}>
+          {doctorGroups.map((group: DoctorGroup, i) => (
+            <List.Accordion
+              style={styles.group}
+              left={props => <List.Icon {...props} icon="account-group" color="azure" />}
+              title={group.name}
+              titleStyle={{ color: 'white' }}
+              id={i} key={i}
+              expanded={i === expandId}
+              onPress={() => handlePress(i, group)}>
 
-            {relationshipList?.length ? (
-              relationshipList.map((relationship: Relationship, k) => (
-                <View key={`${i}-${k}`} style={styles.item}>
-                  <List.Item key={`item${i}-${k}`} 
-                    title={`${relationship.user?.name} ${relationship.user?.gender === 'M' ? '(男)' : (relationship.user?.gender === 'F' ? '(女)' : '')}`}
-                    onPress={() => onSelect(relationship?.user)} />
-                </View>
-              ))
-            ) : (
+              {relationshipList?.length ? (
+                relationshipList.map((relationship: Relationship, k) => (
+                  <View key={`${i}-${k}`} style={styles.item}>
+                    <List.Item key={`item${i}-${k}`}
+                      title={`${relationship.user?.name} ${relationship.user?.gender === 'M' ? '(男)' : (relationship.user?.gender === 'F' ? '(女)' : '')}`}
+                      onPress={() => onSelect(relationship?.user)} />
+                  </View>
+                ))
+              ) : (
                 <Text style={{ backgroundColor: 'lightyellow', paddingTop: 14, paddingBottom: 16, color: 'gray' }}>没有数据</Text>
               )
-            }
-          </List.Accordion>
-        ))
-        }
-      </ScrollView>
-    </>
-  );
+              }
+            </List.Accordion>
+          ))
+          }
+        </ScrollView>
+      </>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
